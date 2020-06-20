83d69de3428bea4ee6ac1d89f6cf0d2e151809fa
Aleksey <nirall@mail.ru>
2020-06-21 02:02:43 +0500

    UI-forms page almost done.

--- src/blocks/appartment-form/appartment-form.js
+++ src/blocks/appartment-form/appartment-form.js
@@ -1,14 +1,16 @@
 try {
-    const dropdownWrapper = document.querySelector(".appartment-form__dropdown-wrapper");
-    dropdownWrapper.onclick = () => {
-        const expandButt = document.querySelector(".appartment-form__dropdown-wrapper span.material-icons");
-        const checkboxes = document.querySelector(".appartment-form__checkboxes");    
-        if (getComputedStyle(checkboxes).opacity !== "0") {
-            checkboxes.classList.add("appartment-form__checkboxes_dissappear");
-            expandButt.classList.add("appartment-form__expand-butt_rotate");
-        } else {
-            checkboxes.classList.remove("appartment-form__checkboxes_dissappear");
-            expandButt.classList.remove("appartment-form__expand-butt_rotate");
+    const dropdownWrapperList = document.getElementsByClassName("appartment-form__dropdown-wrapper");
+    for (let item of dropdownWrapperList) {               
+        item.onclick = () => {            
+            const expandButt = item.parentNode.querySelector("span.material-icons");
+            const checkboxes = item.parentNode.querySelector(".appartment-form__checkboxes");    
+            if (window.getComputedStyle(checkboxes).opacity !== "0") {
+                checkboxes.classList.add("appartment-form__checkboxes_dissappear");
+                expandButt.classList.add("appartment-form__expand-butt_rotate");
+            } else {
+                checkboxes.classList.remove("appartment-form__checkboxes_dissappear");
+                expandButt.classList.remove("appartment-form__expand-butt_rotate");
+            }
         }
     }
 } catch { console.log("There is no dropdown menu.")}
