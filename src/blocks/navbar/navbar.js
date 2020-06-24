75616b1167f03c50b9500ca97b3a044e4b9918fa
Aleksey <nirall@mail.ru>
2020-06-25 01:09:44 +0500

    Responsive navbar

---
+++ src/blocks/navbar/navbar.js
@@ -0,0 +1,10 @@
+try {
+    const moreButt = document.querySelector(".navbar .wrapper-navbar-collapsed .navbar__more");
+    moreButt.onclick = () => {
+        document.querySelector(".navbar .wrapper-navbar-more-list").classList.toggle("wrapper-navbar-more-list_display");
+        document.querySelector(".navbar .wrapper-navbut-buttons").classList.toggle("wrapper-navbut-buttons_display");
+    }
+    
+} catch {
+    console.log("There is no navbar");
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import "./index.scss";
-import "./blocks/datepicker-2f/datepicker.js"
-import "./blocks/datepicker-2f/controller-2f.js"
+
+import "./blocks/navbar/navbar.js";
+import "./blocks/datepicker-2f/datepicker.js";
+import "./blocks/datepicker-2f/controller-2f.js";
 import "./blocks/dropdown/dropdown.js";
 import "./blocks/dropdown-without-butts/dropdown-without-butts.js";
+
 import "./blocks/logo-toxin/logo-toxin.png";
 import "./images/landingpage-bg.jpg";
 import "./images/f.png";
