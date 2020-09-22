37d6232c3447d516b9611e76dfab6aeba5f73f94
Aleksey <nirall@mail.ru>
2020-09-22 09:21:21 +0500

    Issue #4. Merging import rules

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ function importAll(r) {
   r.keys().forEach(r);
 }
 
-importAll(require.context("./", true, /\.(js|s?css)$/i));
-importAll(require.context("./", true, /\.(png|jpe?g|gif|svg)$/i));
+importAll(require.context("./", true, /\.(js|s?css|png|jpe?g|gif|svg)$/i));
 
 import "./blocks/datepicker-2f/datepicker.js";
 import "./blocks/datepicker-2f/datepicker-2f.js";
