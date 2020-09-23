c7c9bed09f985e41c5b93742f483e4bfdb842cf5
Aleksey <nirall@mail.ru>
2020-09-23 09:25:06 +0500

    issue #5. Reafactoring for PerfectPixel

---
+++ src/blocks/navbar/navbar.js
@@ -0,0 +1,15 @@
+class Navbar {
+  static init() {
+    const navbarExpandButt = document.querySelector(".js-navbar__expand-btn");
+    navbarExpandButt.onclick = () => {
+      document.querySelector(".js-navbar__list").classList.toggle("navbar__item_visible-flex");
+      document.querySelector(".js-navbar__buttons").classList.toggle("navbar__item_visible-flex");
+    }
+  }
+}
+
+try {
+  Navbar.init();
+} catch {
+  console.log("There is no navbar");
+}
