f06f2f6ed3088bd112b72e44bd8bc0ea460155b4
Aleksey <nirall@mail.ru>
2020-09-23 11:42:42 +0500

    Issue #8. Refactoring for BEM rules

--- src/blocks/navbar/navbar.js
+++ src/blocks/navbar/navbar.js
@@ -2,8 +2,8 @@ class Navbar {
   static init() {
     const navbarExpandButt = document.querySelector(".js-navbar__expand-btn");
     navbarExpandButt.onclick = () => {
-      document.querySelector(".js-navbar__list").classList.toggle("navbar__item_visible-flex");
-      document.querySelector(".js-navbar__buttons").classList.toggle("navbar__item_visible-flex");
+      document.querySelector(".js-navbar__list").classList.toggle("navbar__item_visible_flex");
+      document.querySelector(".js-navbar__buttons").classList.toggle("navbar__item_visible_flex");
     }
   }
 }
